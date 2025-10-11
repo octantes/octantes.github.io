82c46a793fbc2d2028d3fbc7dbe9fc0584bbbb81
kaste <facugerbino@gmail.com>
2025-10-11 09:25:18 -0300

    route changes

--- src/01/content.vue
+++ src/01/content.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, watch, nextTick, computed } from 'vue'
 import { useRoute } from 'vue-router'
-import Shader from '../04/shader.vue'
+import Shader from '../03/shader.vue'
 import Portada from '../02/portada.vue'
 import A2 from '../02/A2.vue'
 import S6 from '../02/S6.vue'
