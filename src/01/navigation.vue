d2c715a240970c46691c90aa4eb970155c822715
kaste <facugerbino@gmail.com>
2025-10-19 11:31:55 -0300

    final query

--- src/01/navigation.vue
+++ src/01/navigation.vue
@@ -505,8 +505,8 @@ table {
   .tablediv { padding-left: 0rem; padding-right: 0rem;}
 
   table .col-fecha  { width: 02%; }
-  table .col-titulo { width: 73%; }
-  table .col-tags   { width: 25%; }
+  table .col-titulo { width: 68%; }
+  table .col-tags   { width: 30%; }
 
   .tabs button        { display: none; }
   .tabs button.active { display: flex; }
