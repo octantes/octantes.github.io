6761adf134f60a29736fd9ceabca228ed570d7b3
kaste <facugerbino@gmail.com>
2025-11-16 09:52:55 -0300

    test filter center keep

--- src/04/store.js
+++ src/04/store.js
@@ -173,8 +173,7 @@ export const useStore = defineStore('store', () => {
     activeFilter.value = filter
     currentPage.value = 1
 
-    if (filter === 'full') { isCentered.value = false }
-    else if (!isCentered.value) { isCentered.value = true }
+    if (filter !== 'full' && !isCentered.value) { isCentered.value = true }
 
     if (routerInstance) {
 
