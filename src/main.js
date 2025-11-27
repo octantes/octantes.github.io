fada76e5bda62e10a2619bdbeb39f122eecd6cb2
kaste <facugerbino@gmail.com>
2025-11-27 18:08:03 -0300

    test google analytics

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './04/router.js'
 import App from './octantes.vue'
+import gTag from 'vue-gtag'
 import './04/baseline.css'
 import './04/content.css'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).mount('#octantes')
+createApp(App).use(router).use(pinia).use(gTag, { config: { id: 'G-8LPD65HMJ8' } }, router).mount('#octantes')
