4f929563618cebe05372246d68ae168591c09be8
kaste <facugerbino@gmail.com>
2025-11-27 19:03:58 -0300

    test analytics

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './04/router.js'
 import App from './octantes.vue'
-import * as Gtag from 'vue-gtag'
+import Gtag from 'vue-gtag'
 import './04/baseline.css'
 import './04/content.css'
 
 const pinia = createPinia()
+const app = createApp(App)
 
-createApp(App).use(router).use(pinia).use(Gtag.default || Gtag, { config: { id: 'G-8LPD65HMJ8' } }, router).mount('#octantes')
+app.use(router)
+app.use(pinia)
+
+app.use(Gtag, { config: { id: 'G-8LPD65HMJ8' } }, router)
+
+app.mount('#octantes')
