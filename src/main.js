71d60c9135b627514da575d404831b56d568e43a
kaste <facugerbino@gmail.com>
2025-11-27 18:14:17 -0300

    test fix

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './04/router.js'
 import App from './octantes.vue'
-import VueGtag from 'vue-gtag'
+import * as Gtag from 'vue-gtag'
 import './04/baseline.css'
 import './04/content.css'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).use(VueGtag, { config: { id: 'G-8LPD65HMJ8' } }, router).mount('#octantes')
+createApp(App).use(router).use(pinia).use(Gtag.default || Gtag, { config: { id: 'G-8LPD65HMJ8' } }, router).mount('#octantes')
